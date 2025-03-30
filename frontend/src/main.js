4037b6a56fcdfb215a024de1fa979fa16e2db794
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-03-30 11:05:21 +0700

    Menginisialisasi dan mengkonfigurasi aplikasi (Pinia, Router)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,24 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+// impor fungsi 'createPinia' untuk state management dengan Pinia
+import { createPinia } from 'pinia';
 
-createApp(App).mount('#app')
+// impor fungsi 'createApp' untuk membuat aplikasi Vue
+import { createApp } from 'vue';
+
+// impor router yang digunakan untuk navigasi antar halaman
+import router from './router';
+
+import App from './App.vue';
+
+// membuat instance aplikasi Vue berdasarkan komponen utama 'App.vue'
+const app = createApp(App);
+
+// menggunakan Pinia sebagai state management dalam aplikasi
+app.use(createPinia());
+
+// menggunakan Vue Router untuk navigasi antar halaman
+app.use(router);
+
+// memasang aplikasi pada elemen dengan id 'app' di file index.html
+app.mount('#app');

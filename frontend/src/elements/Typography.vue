61ade129a67ffea1862f98128759bf7eafa0d266
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-04-04 08:46:49 +0700

    Membuat element typography

---
+++ frontend/src/elements/Typography.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { defineProps } from 'vue';
+
+// mendeklarasikan properti yang dapat diterima oleh komponen ini dari parent
+defineProps({
+  classText: {
+    type: String,
+    default: ''
+  } // ini nama propsnya jangan "class" karena akan bentrok dengan "class" di html
+});
+</script>
+
+<template>
+  <p :class="`font-inter text-base ${className}`">
+    <!-- 'slot' digunakan agar konten tombol bisa disesuaikan dari parent -->
+    <slot />
+  </p>
+</template>

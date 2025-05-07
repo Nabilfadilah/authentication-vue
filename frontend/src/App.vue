2850c1cece21801142b8ad8c0b13a7f412c13279
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-05-07 20:29:08 +0700

    Memberikan class style untuk app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,3 @@
-<script setup>
-</script>
-
 <template>
-  <!-- membungkus seluruh tampilan dalam div dengan tinggi minimum layar -->
-  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
-    <!-- komponen router-view digunakan untuk menampilkan halaman berdasarkan route -->
-    <router-view />
-  </div>
+  <router-view /> <!-- akan render MainLayout atau AuthLayout sesuai route -->
 </template>
-
-<style scoped>
-</style>

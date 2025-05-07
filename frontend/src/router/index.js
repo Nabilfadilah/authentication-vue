88437eb2f464e3e379dc389a132f4aae16a1aef6
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-05-07 20:30:37 +0700

    Menambahkan componeny authLayout dan mainLayout pada router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,19 +14,34 @@ import BiodataRouter from './BiodataRouter.vue';
 import authGuard from '../guards/authGuard';
 import BiodataCreate from '@/components/BiodataCreate.vue';
 import BiodataEdit from '@/components/BiodataEdit.vue';
+import MainLayout from '@/layouts/MainLayout.vue';
+import AuthLayout from '@/layouts/AuthLayout.vue';
 
 // daftar route dalam aplikasi
 const routes = [
-  { path: '/login', component: Login },         // route untuk halaman login
-  { path: '/register', component: Register },   // route untuk halaman register
-  { path: '/dashboard', component: Dashboard, beforeEnter: authGuard }, // route dashboard hanya bisa diakses jika sudah login
-  { path: '/users', component: UserList, beforeEnter: authGuard }, // route userlist hanya bisa diakses jika sudah login
-  { path: '/users/:id', component: UserEdit, beforeEnter: authGuard },
-  { path: '/admin/reset-password/:id', component: ResetPassword, beforeEnter: authGuard},  
-  { path: '/biodata', component: BiodataRouter, beforeEnter: authGuard },
-  { path: '/biodata/create', component: BiodataCreate, beforeEnter: authGuard },
-  { path: '/biodata/edit/:id', component: BiodataEdit, beforeEnter: authGuard },
-];
+  {
+    path: '/',
+    component: MainLayout,
+    beforeEnter: authGuard,
+    children: [
+      { path: '/dashboard', component: Dashboard, beforeEnter: authGuard }, // route dashboard hanya bisa diakses jika sudah login
+      { path: '/users', component: UserList, beforeEnter: authGuard }, // route userlist hanya bisa diakses jika sudah login
+      { path: '/users/:id', component: UserEdit, beforeEnter: authGuard },
+      { path: '/admin/reset-password/:id', component: ResetPassword, beforeEnter: authGuard},  
+      { path: '/biodata', component: BiodataRouter, beforeEnter: authGuard },
+      { path: '/biodata/create', component: BiodataCreate, beforeEnter: authGuard },
+      { path: '/biodata/edit/:id', component: BiodataEdit, beforeEnter: authGuard },
+    ]
+  },
+  {
+    path: '/',
+    component: AuthLayout, // layout tanpa sidebar/nav
+    children: [
+      { path: '/login', component: Login },         // route untuk halaman login
+      { path: '/register', component: Register },   // route untuk halaman register
+    ]
+  }
+]
 
 // membuat instance router dengan mode history agar URL terlihat lebih bersih
 const router = createRouter({
